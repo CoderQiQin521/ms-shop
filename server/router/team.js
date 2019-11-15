358b006f358f0cb10a97ad5a58561e23c5f58bca
CoderQiQin521 <coderqiqin@aliyun.com>
2019-11-15 20:44:43 +0800

    响应汉字信息

--- server/router/team.js
+++ server/router/team.js
@@ -5,4 +5,9 @@ router.get('/team', function (req, res, next) {
   res.end('hello qiqin')
 })
 
+router.get('/qiqin', function (req, res, next) {
+  res.writeHead(200, { 'Content-Type': 'text/plain;charset=UTF-8' })
+  res.end('人生际遇')
+})
+
 module.exports = router
